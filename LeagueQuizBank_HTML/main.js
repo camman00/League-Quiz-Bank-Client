ffcb123b1b43a8d9838d921edf947e2dd0244d7d
camman00 <league@WTS5.attlocal.net>
2018-07-29 14:24:36 -0700

    Touched up some things
    Fixed MaxWidth

--- LeagueQuizBank_HTML/main.js
+++ LeagueQuizBank_HTML/main.js
@@ -1,2 +1,15 @@
-//Somestuff
-
+console.log('Page Initalized');
+window.addEventListener('resize',pageSize);
+var originalWidth = '75%';
+function pageSize() {
+    var leagueImageMaxWidth = 1200;
+    var windowWidth = window.innerWidth;
+    var leagueImage = window.document.getElementById('league_picture');
+    //540
+    if(windowWidth > leagueImageMaxWidth) {
+        leagueImage.style.width = '540px';
+    }
+    else {
+        leagueImage.style.width = originalWidth;
+    }
+}
